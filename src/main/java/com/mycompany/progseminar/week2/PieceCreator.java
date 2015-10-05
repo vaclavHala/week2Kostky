61c9168c5ee11c1aadc4779e18314a216132839d
cypo721 <cyprian.patrik@gmail.com>
2015-10-05 11:46:36 +0200

    PieceCreator from string

---
+++ src/main/java/com/mycompany/progseminar/week2/PieceCreator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.progseminar.week2;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.mycompany.progseminar.week2.Point;
+/**
+ *
+ * @author pato
+ */
+public class PieceCreator {
+    private List<Point> points = new ArrayList<>();
+    
+    public Piece create(String piece) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0 ; i < piece.length() ; i++ ) {
+            if (piece.charAt(i) == '*') points.add(new Point(x,y));
+            x++;
+            if (piece.charAt(i) == '\n') {
+                y++;
+                x = 0;
+            }
+        }      
+        return new Piece(points);
+    }      
+}
