c29b95784b3215c7df96f211aa0d30773da1b7d4
Vaclav Hala <xhala3@nymfe14.fi.muni.cz>
2015-10-12 09:48:32 +0200

    nice printout

--- src/test/java/com/mycompany/progseminar/week2/AlgoTest.java
+++ src/test/java/com/mycompany/progseminar/week2/AlgoTest.java
@@ -8,6 +8,15 @@ public class AlgoTest {
     @Test
     //2.5
     public void testCorrectSizes() {
+        Algo.doIt(1).stream().forEach(p -> System.out.println(p.render()));
+        System.out.println("-----------------------------");
+        Algo.doIt(2).stream().forEach(p -> System.out.println(p.render()));
+        System.out.println("-----------------------------");
+        Algo.doIt(3).stream().forEach(p -> System.out.println(p.render()));
+        System.out.println("-----------------------------");
+        Algo.doIt(4).stream().forEach(p -> System.out.println(p.render()));
+        System.out.println("-----------------------------");
+        Algo.doIt(5).stream().forEach(p -> System.out.println(p.render()));
         //assertEquals(35, Algo.doIt(6).size());
         //assertEquals(369, Algo.doIt(8).size());
         //assertEquals(1285, Algo.doIt(9).size());
